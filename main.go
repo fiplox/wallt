bb84a2a0d626f94ea126e93559558ca2e8252dba
fiplox <56274824+fiplox@users.noreply.github.com>
2020-07-25 16:12:37 +0200

    completely reworked with flags

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -12,14 +13,31 @@ import (
 )
 
 func main() {
-	args := os.Args[1:]
+	var wallpaperPath, mode, HHMM string
 
-	files, err := ioutil.ReadDir(args[0])
+	flag.StringVar(&wallpaperPath, "path", "", "Specify absolute path to wallpaper folder. Required.")
+	flag.StringVar(&mode, "mode", "auto", "Specify mode of use. Mods available: auto (default), interval, time.")
+	flag.StringVar(&HHMM, "time", "", "Specify the interval of time to change wallpaper. Required if -mode=interval.")
+
+	flag.Parse()
+
+	if wallpaperPath == "" || (mode == "interval" && HHMM == "") || (mode == "auto" && HHMM != "") || (mode == "time" && HHMM != "") {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		return
+	}
+
+	if wallpaperPath[len(wallpaperPath)-1] != '/' {
+		wallpaperPath += "/"
+	}
+
+	files, err := ioutil.ReadDir(wallpaperPath)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
+	// get file names.
 	var fileNames []string
 	var i int
 	for _, f := range files {
@@ -30,149 +48,108 @@ func main() {
 		i++
 	}
 
-	// Divide day by number of pictures in the folder.
-	if len(args) == 1 {
+	i = 0
+	conf, err := os.OpenFile(wallpaperPath+".index", os.O_RDWR, 0755)
+	if err != nil {
+		conf, err := os.Create(wallpaperPath + ".index")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		conf.WriteString("0")
+	} else {
+		_, err = fmt.Fscanf(conf, "%d", &i)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	conf.Close()
+
+	switch mode {
+	case "auto":
 		var delay int = 86400 / len(fileNames)
-		h := (delay / 3600)
-		m := (delay - (3600 * h)) / 60
-		s := (delay - (3600 * h) - (m * 60))
-		clock := make([][]int, 3)
-		for i := range clock {
-			clock[i] = make([]int, len(fileNames))
-		}
-		clock[0][1] += h
-		clock[1][1] += m
-		clock[2][1] += s
-
-		for i := 2; i < len(fileNames); i++ {
-			clock[0][i] = h + clock[0][i-1]
-			clock[1][i] = m + clock[1][i-1]
-			clock[2][i] = s + clock[2][i-1]
-			if clock[2][i] >= 60 {
-				clock[2][i] -= 60
-				clock[1][i] += 1
-			}
-			if clock[1][i] >= 60 {
-				clock[1][i] -= 60
-				clock[0][i] += 1
-			}
-			if clock[0][i] >= 24 {
-				clock[0][i] -= 24
-			}
-
-		}
-		var i int
-		for I, t := range clock[0] {
-			now := time.Now()
-			if now.Hour() > t && now.Hour() < clock[0][I+1] {
-				// set wallpaper to files[i] and calculate next sleep time.
-				wallpaper.SetFromFile(args[0] + fileNames[i])
-				i = I + 1
-				break
-			}
-		}
-
-		if i == 0 {
-			// set wallpaper to files[0]
-			wallpaper.SetFromFile(args[0] + fileNames[0])
-		}
-
 		for {
 			now := time.Now()
 			if i >= len(fileNames) {
 				i -= len(fileNames)
 			}
-			//	next := time.Date(now.Year(), now.Month(), now.Day(), clock[0][i], clock[1][i], clock[2][i], 0, now.Location())
-			next := now.Add(time.Second * time.Duration((clock[0][i]*3600 + clock[1][i]*60 + clock[2][i])))
-			//set wallpaper to files[i].
-			wallpaper.SetFromFile(args[0] + fileNames[i])
-			i++
-			time.Sleep(time.Until(next))
-		}
-	}
 
-	// Set time manually.
-	if len(args) > 1 {
-		if args[1] == "-t" || args[1] == "--set-time" {
-
-			times := make([]int, len(args[2:]))
-			for i, t := range args[2:] {
-				times[i], err = strconv.Atoi(t)
-				if err != nil {
-					fmt.Println(err)
-					os.Exit(1)
-				}
-			}
-
-			now := time.Now()
-			var i int
-			for I, t := range times {
-				if now.Hour() > t && now.Hour() < t {
-					// set wallpaper to files[i] and calculate next sleep time.
-					wallpaper.SetFromFile(args[0] + fileNames[i])
-					fmt.Println(files[I].Name())
-					i = I + 1
-					break
-				}
-			}
-			if i == 0 {
-				// set wallpaper to files[0]
-				wallpaper.SetFromFile(args[0] + fileNames[0])
-				fmt.Println(args[0] + files[i].Name())
-			}
-			for {
-				if i >= len(fileNames) {
-					i -= len(fileNames)
-				}
-				next := time.Date(now.Year(), now.Month(), now.Day(), times[i], 0, 0, 0, now.Location())
-				//set wallpaper to files[i].
-				wallpaper.SetFromFile(args[0] + fileNames[i])
-				fmt.Println(time.Until(next))
-				i++
-				time.Sleep(time.Until(next))
-			}
-		}
-
-		// Set interval in HH:mm format.
-		if args[1] == "-i" || args[1] == "--set-interval" {
-			re := regexp.MustCompile(`^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$`)
-			if !re.MatchString(args[2]) {
-				fmt.Println(args[2], ": Wrong format.")
+			next := now.Add(time.Second * time.Duration(delay))
+			err := wallpaper.SetFromFile(wallpaperPath + fileNames[i])
+			if err != nil {
+				fmt.Println(err)
 				os.Exit(1)
 			}
-			var i int
-			conf, err := os.OpenFile(args[0]+".index", os.O_RDWR, 0755)
+			fmt.Println("next in:", time.Until(next))
+			i++
+			ioutil.WriteFile(wallpaperPath+".index", []byte(strconv.Itoa(i)), 644)
+			time.Sleep(time.Until(next))
+		}
+	case "interval":
+		re := regexp.MustCompile(`^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$`)
+		if !re.MatchString(HHMM) {
+			fmt.Printf("%s: Wrong format.", HHMM)
+			os.Exit(1)
+		}
+
+		h, _ := strconv.Atoi(string(HHMM[0]) + string(HHMM[1]))
+		m, _ := strconv.Atoi(string(HHMM[3]) + string(HHMM[4]))
+		m += h * 60
+
+		for {
+			if i > len(fileNames) {
+				i = 0
+			}
+			now := time.Now()
+			next := now.Add(time.Minute * time.Duration(m))
+			err := wallpaper.SetFromFile(wallpaperPath + fileNames[i])
 			if err != nil {
-				conf, err := os.Create(args[0] + ".index")
-				if err != nil {
-					fmt.Println(err)
-					os.Exit(1)
-				}
-				conf.WriteString("0")
-			} else {
-				_, err = fmt.Fscanf(conf, "%d", &i)
-				if err != nil {
-					fmt.Println(err)
-					os.Exit(1)
-				}
+				fmt.Println(err)
+				os.Exit(1)
 			}
-			conf.Close()
-			h, _ := strconv.Atoi(string(args[2][0]) + string(args[2][1]))
-			m, _ := strconv.Atoi(string(args[2][3]) + string(args[2][4]))
-			m += h * 60
-			for {
-				if i > len(fileNames) {
-					i = 0
-				}
-				now := time.Now()
-				next := now.Add(time.Minute * time.Duration(m))
-				// set wallpaper to fileNames[i]
-				wallpaper.SetFromFile(args[0] + fileNames[i])
-				fmt.Println("next in:", time.Until(next), len(fileNames))
-				i++
-				ioutil.WriteFile(args[0]+".index", []byte(strconv.Itoa(i)), 0644)
-				time.Sleep(time.Until(next))
+			fmt.Println("next in:", time.Until(next))
+			i++
+			ioutil.WriteFile(wallpaperPath+".index", []byte(strconv.Itoa(i)), 644)
+			time.Sleep(time.Until(next))
+		}
+	case "time":
+		times := make([]int, len(flag.Args()))
+		for I, t := range flag.Args() {
+			times[I], err = strconv.Atoi(t)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
 			}
+		}
+
+		now := time.Now()
+		i = 0
+		for I := 0; I < len(times)-1; I++ {
+			if now.Hour() > times[I] && now.Hour() < times[I+1] {
+				i = I + 1
+				break
+			}
+		}
+
+		for {
+			now = time.Now()
+			if i >= len(times) {
+				i = 0
+			}
+
+			next := time.Date(now.Year(), now.Month(), now.Day(), times[i], 0, 0, 0, now.Location())
+			if time.Until(next) < time.Duration(0) {
+				next = next.AddDate(0, 0, 1)
+			}
+			err := wallpaper.SetFromFile(wallpaperPath + fileNames[i])
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println("next in:", time.Until(next))
+			i++
+			time.Sleep(time.Until(next))
 		}
 	}
 }
